cabd8e940da71945954f1b757d6fcb6d8ee5d9e2
LeHenschel <leonie.henschel@dzne.de>
2022-07-06 13:35:29 -0400

    Removed redundant code snippets and added flexible block size

--- model/RCVNet.py
+++ model/RCVNet.py
@@ -35,6 +35,7 @@ def multi_gpu_check(gpu_map, l_name, *args):
         return args[0]
     return args
 
+
 class RCVNet(nn.Module):
 
     """
@@ -55,6 +56,7 @@ class RCVNet(nn.Module):
         self.input_shape = params['input_shape']
         self.patch_size = params['patch_size']
         self.gen_random = params['gen_random']
+        self.num_blocks = params["num_blocks"] - 1
 
         # Choose sub model
         if params['sub_model_name'] == 'vnet':
@@ -74,161 +76,85 @@ class RCVNet(nn.Module):
         else:
             raise ValueError(f"{params['sub_model_name']} does not exist.")
 
-        # Artefact from another file. Unfortunately was not removed and is a dangling layer in many of the saved models
-        # i.e. registered as a parameter, but never used
-        self.down_input_lower = nn.Sequential(
-                nn.Conv3d(in_channels=params['in_channels'], out_channels=4*params['out_channels'],
-                          kernel_size=(4,4,4), padding=0, stride=4),
-                nn.GroupNorm(num_groups=4, num_channels=4*params['out_channels']),
-                nn.PReLU()
-            )
-
         # Start model creation
         # in_channels: 16, out_channels: 16
-        self.encoder_block_1 = EncoderBlock(params)
+        # Parameters for the Descending Arm
+        self.encoderbase = nn.ModuleList()
+        for i in range(params["num_blocks"] - 1):
+            if i > 0:
+                params['input'] = False
+                params['create_layer_1'] = True
+                params['in_channels'] = params['out_channels'] * 2  # 32, 64, 128, 256
+                params['out_channels'] = params['out_channels'] * 2  # 32, 64, 128, 256
 
-        params['input'] = False
-        params['create_layer_1'] = True
-        params['in_channels'] = params['out_channels'] * 2 # 32
-        params['out_channels'] = params['out_channels'] * 2 # 32
-        self.encoder_block_2 = EncoderBlock(params)
+            self.encoderbase.append(EncoderBlock(params))
 
-        params['create_layer_2'] = True
-        params['in_channels'] = params['out_channels'] * 2 # 64
-        params['out_channels'] = params['out_channels'] * 2 # 64
-        self.encoder_block_3 = EncoderBlock(params)
+            params['create_layer_2'] = True
 
-        params['in_channels'] = params['out_channels'] * 2 # 128
-        params['out_channels'] = params['out_channels'] * 2 # 128
-        self.encoder_block_4 = EncoderBlock(params)
+        params['in_channels'] = params['out_channels'] * 2  # 32, 64, 128, 256
+        params['out_channels'] = params['out_channels'] * 2
 
-        params['in_channels'] = params['out_channels'] * 2 # 256
-        params['out_channels'] = int(params['out_channels'] * 2) # 256
         self.bottleneck_block = BottleNeck(params)
+        # Parameters for Ascending Arm
+        params['in_channels'] = params['out_channels'] + int(params['out_channels'] / 2)  # 256 + 128 = 384
+        self.decoderbase = nn.ModuleList([DecoderBlock(params)])
+        for i in range(params["num_blocks"]-2):
 
-        enc_channels = 128
-        params['in_channels'] = params['out_channels'] + enc_channels # 256 + 128
-        params['out_channels'] = params['out_channels'] # 256
-        self.decoder_block_4 = DecoderBlock(params)
+            params['out_channels'] = int(params['out_channels'] / 2)  # 128, 64, 32
+            params['in_channels'] = params['out_channels'] + int(params['out_channels']/2)  # 128 + 64 = 192, 64 + 32 = 96
+            if i == params["num_blocks"] - 4: # second to last layer
+                params['create_layer_2'] = False
+            if i == params["num_blocks"] - 3:
+                params['create_layer_1'] = False
+                params['out'] = True
+            self.decoderbase.append(DecoderBlock(params))
 
-        enc_channels = int(enc_channels/2)
-        params['in_channels'] = int(params['out_channels']/2) + enc_channels # 128 + 64
-        params['out_channels'] = int(params['out_channels'] / 2)  # 128
-        self.decoder_block_3 = DecoderBlock(params)
-
-        enc_channels = int(enc_channels/2)
-        params['in_channels'] = int(params['out_channels'] / 2) + enc_channels  # 64 + 32
-        params['out_channels'] = int(params['out_channels'] / 2) # 64
-        params['create_layer_2'] = False
-        self.decoder_block_2 = DecoderBlock(params)
-
-        enc_channels = int(enc_channels/2)
-        params['in_channels'] = int(params['out_channels'] / 2) + enc_channels # 32 + 16
-        params['out_channels'] = int(params['out_channels'] / 2) # 32
-        params['create_layer_1'] = False
-        params['out'] = True
-        self.decoder_block_1 = DecoderBlock(params)
+        # Logits
         params['out'] = False
-
         self.output_block = nn.Conv3d(in_channels=params['out_channels'], out_channels=params['num_classes'],
                                       kernel_size = (1, 1, 1), stride = 1, padding = 0)
 
         self.gpu_map = params['gpu_map']
 
-    def forward(self, x):
-        if self.training:
-            return self.train_forward(x)
-        else:
-            return self.eval_forward(x)
-
-
-    def train_forward(self, x):
-        """
-        Standard VNet Architecture
-        """
-
+    def random_patch(self, x):
         # Generate Random Coordinates if needed
-        if self.gen_random: # For usage by QuadNet
+        if self.gen_random:  # For usage by QuadNet
             self.coords = make_rand_coords(self.input_shape, self.patch_size)
         assert self.coords is not None
 
         # Cropped Patch for the part of encoder
         x_upper = x[..., self.coords[0]:self.coords[0] + self.patch_size[0],
-                  self.coords[1]:self.coords[1] + self.patch_size[1],
-                  self.coords[2]:self.coords[2] + self.patch_size[2]]
+                    self.coords[1]:self.coords[1] + self.patch_size[1],
+                    self.coords[2]:self.coords[2] + self.patch_size[2]]
+        return x_upper
 
-        # Running Encoder side of network
-        x_upper = multi_gpu_check(self.gpu_map, 'encoder_block_1', x_upper)
-        x_upper_res_1, x_down_1 = self.encoder_block_1(x_upper)
-
-        x_down_1 = multi_gpu_check(self.gpu_map, 'encoder_block_2', x_down_1)
-        x_upper_res_2, x_down_2 = self.encoder_block_2(x_down_1)
-
-        x_down_2 = multi_gpu_check(self.gpu_map, 'encoder_block_3', x_down_2)
-        x_lower_res_3, x_down_3 = self.encoder_block_3(x_down_2)
-
-        x_down_3 = multi_gpu_check(self.gpu_map, 'encoder_block_4', x_down_3)
-        x_lower_res_4, x_down_4 = self.encoder_block_4(x_down_3)
-
-        # Running bottleneck
-        x_down_4 = multi_gpu_check(self.gpu_map, 'bottleneck_block', x_down_4)
-        x_bottleneck = self.bottleneck_block(x_down_4)
-
-        # Run decoder
-        x_lower_res_4, x_bottleneck = multi_gpu_check(self.gpu_map, 'decoder_block_4', x_lower_res_4, x_bottleneck)
-        x_up_4 = self.decoder_block_4(x_lower_res_4, x_bottleneck)
-
-        x_lower_res_3, x_up_4 = multi_gpu_check(self.gpu_map, 'decoder_block_3', x_lower_res_3, x_up_4)
-        x_up_3 = self.decoder_block_3(x_lower_res_3, x_up_4)
-
-        x_upper_res_2, x_up_3 = multi_gpu_check(self.gpu_map, 'decoder_block_2', x_upper_res_2, x_up_3)
-        x_up_2 = self.decoder_block_2(x_upper_res_2, x_up_3)
-
-        x_upper_res_1, x_up_2 = multi_gpu_check(self.gpu_map, 'decoder_block_1', x_upper_res_1, x_up_2)
-        x_last = self.decoder_block_1(x_upper_res_1, x_up_2)
-
-        x_last = multi_gpu_check(self.gpu_map, 'output_block', x_last)
-        out = self.output_block(x_last)
-
-        return out
-
-
-    def eval_forward(self, x):
+    def forward(self, x):
         """
         Standard VNet Architecture
         """
+        if self.training:
+            x = self.random_patch(x)
 
-        # Standard evaluation. No patch extraction
-        x_upper = multi_gpu_check(self.gpu_map, 'encoder_block_1', x)
-        x_upper_res_1, x_down_1 = self.encoder_block_1(x_upper)
+        # Running Encoder side of network
+        encode, skip, decode = [x], [], []
+        for i in range(self.num_blocks):
+            input = multi_gpu_check(self.gpu_map, 'encoder_block_' + str(i + 1), encode[i])
+            skip_encoder, encoder_output  = self.encoderbase[i](input)
+            encode.append(encoder_output)
+            skip.append(skip_encoder)
 
-        x_down_1 = multi_gpu_check(self.gpu_map, 'encoder_block_2', x_down_1)
-        x_upper_res_2, x_down_2 = self.encoder_block_2(x_down_1)
+        # Running bottleneck
+        input = multi_gpu_check(self.gpu_map, 'bottleneck_block', encode[-1])
+        decode.append(self.bottleneck_block(input))
 
-        x_down_2 = multi_gpu_check(self.gpu_map, 'encoder_block_3', x_down_2)
-        x_lower_res_3, x_down_3 = self.encoder_block_3(x_down_2)
+        # Run decoder
+        for i in range(self.num_blocks):
+            skip_in, decode_in = multi_gpu_check(self.gpu_map, 'decoder_block_' + str(self.num_blocks - 1), skip[-i-1], decode[i])
+            decode.append(self.decoderbase[i](skip_in, decode_in))
 
-        x_down_3 = multi_gpu_check(self.gpu_map, 'encoder_block_4', x_down_3)
-        x_lower_res_4, x_down_4 = self.encoder_block_4(x_down_3)
-
-        # Running bottlenext and decoder
-        x_down_4 = multi_gpu_check(self.gpu_map, 'bottleneck_block', x_down_4)
-        x_bottleneck = self.bottleneck_block(x_down_4)
-
-        x_lower_res_4, x_bottleneck = multi_gpu_check(self.gpu_map, 'decoder_block_4', x_lower_res_4, x_bottleneck)
-        x_up_4 = self.decoder_block_4(x_lower_res_4, x_bottleneck)
-
-        x_lower_res_3, x_up_4 = multi_gpu_check(self.gpu_map, 'decoder_block_3', x_lower_res_3, x_up_4)
-        x_up_3 = self.decoder_block_3(x_lower_res_3, x_up_4)
-
-        x_upper_res_2, x_up_3 = multi_gpu_check(self.gpu_map, 'decoder_block_2', x_upper_res_2, x_up_3)
-        x_up_2 = self.decoder_block_2(x_upper_res_2, x_up_3)
-
-        x_upper_res_1, x_up_2 = multi_gpu_check(self.gpu_map, 'decoder_block_1', x_upper_res_1, x_up_2)
-        x_last = self.decoder_block_1(x_upper_res_1, x_up_2)
-
-        x_last = multi_gpu_check(self.gpu_map, 'output_block', x_last)
-        out = self.output_block(x_last)
+        # Run logits
+        decode_in = multi_gpu_check(self.gpu_map, 'output_block', decode[-1])
+        out = self.output_block(decode_in)
 
         return out
 
@@ -324,7 +250,9 @@ if __name__ == "__main__":
               # 'F_l': None,
               # 'F_int': None
               'gen_random' : True,
-              'gpu_map':{}
+              'gpu_map':{},
+              'num_blocks': 5,
+              'sub_model_name': "vnet"
               }
 
     m = RCVNet(params=params).cuda()
